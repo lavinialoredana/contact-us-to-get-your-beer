27c15cc02f7fd9a3709af324b28c6ac7a0bf58ed
LAVINIA SIRBULESCU <lavinia.loredana@thoughtworks.com>
2022-12-23 10:58:54 +0200

    Project cleanup

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,8 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(
+    /Order now our selection of punk beers/i
+  );
   expect(linkElement).toBeInTheDocument();
 });
