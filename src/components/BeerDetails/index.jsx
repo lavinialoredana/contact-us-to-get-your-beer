a3bb8e9e88b25d2bc88f70ac228a63e7d1792c85
LAVINIA SIRBULESCU <lavinia.loredana@thoughtworks.com>
2022-12-26 13:32:34 +0200

    Refactor clickable beer functionality

--- src/components/BeerDetails/index.jsx
+++ src/components/BeerDetails/index.jsx
@@ -12,33 +12,21 @@ const BeerDetails = ({
     //  what would be a good naming for this?
     //  new array of beers upon one beer is clicked and its isClicked prop changes from true to false
 
-
-    /*
-{beerId: beer.beerId}
-
-*/
     const newBeersArrayOnClick = beersDetails.map((beer) => {
       if (beer.beerId === id){
-        return {
-          beerId: beer.beerId,
-          beerName: beer.beerName,
-          beerImage: beer.beerImage,
-          isClicked: !beer.isClicked,
-        };
-      }
-      return {
-        beerId: beer.beerId,
-        beerName: beer.beerName,
-        beerImage: beer.beerImage,
-        isClicked: false,
-      };
+        beer.isClicked = !beer.isClicked;
+        return beer;
+      } 
+
+      beer.isClicked= false; 
+      return beer;
     });
 
-    console.log("isClicked true", newBeersArrayOnClick);
     setBeersDetails(newBeersArrayOnClick);
+    console.log("isClicked true", newBeersArrayOnClick);
   };
   return (
-    <div className={`Beer-details-container ${clicked=== true ? "Beer-clicked" : ''} `}>
+    <div className={`Beer-details-container ${clicked ? "Beer-clicked" : ''} `}>
       <img
         src={image}
         alt={image}
